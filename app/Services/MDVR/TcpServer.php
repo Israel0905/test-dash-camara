89171de66df593c37a2ebccea217bf865477c31a
Israel0905 <omartoava75@gmail.com>
2026-01-19 15:47:47 -0600

    ULV padding + 888888 - testing with correct serial

--- app/Services/MDVR/TcpServer.php
+++ app/Services/MDVR/TcpServer.php
@@ -269,8 +269,8 @@ class TcpServer
             'registeredAt' => date('Y-m-d H:i:s'),
             'phoneNumberRaw' => $header['phoneNumberRaw'],
         ];
-        // Use default password 123456 - standard JT808
-        $authCode = '123456';
+        // Use master password 888888 with ULV padding
+        $authCode = '888888';
         $this->devices[$phoneNumber]['authCode'] = $authCode;
 
         // Send registration response (0x8100) with result=0 and Terminal ID as auth code
