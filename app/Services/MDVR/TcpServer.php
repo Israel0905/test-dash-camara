ebe863fea071dee447ca464207d6e5200f5934de
Israel0905 <omartoava75@gmail.com>
2026-01-19 16:23:40 -0600

    Length prefix + WiFi password - final attempt

--- app/Services/MDVR/TcpServer.php
+++ app/Services/MDVR/TcpServer.php
@@ -269,8 +269,8 @@ class TcpServer
             'registeredAt' => date('Y-m-d H:i:s'),
             'phoneNumberRaw' => $header['phoneNumberRaw'],
         ];
-        // Use 888888 with length prefix format
-        $authCode = '888888';
+        // Use WiFi password from label with length prefix format
+        $authCode = '1122334455';
         $this->devices[$phoneNumber]['authCode'] = $authCode;
 
         // Send registration response (0x8100) with result=0 and Terminal ID as auth code
