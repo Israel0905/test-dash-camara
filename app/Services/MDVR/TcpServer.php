f10f639ee9e525335de396f08a37c1299dc7fc67
Israel0905 <omartoava75@gmail.com>
2026-01-19 16:05:08 -0600

    ULV padding + S/N from camera label

--- app/Services/MDVR/TcpServer.php
+++ app/Services/MDVR/TcpServer.php
@@ -269,8 +269,8 @@ class TcpServer
             'registeredAt' => date('Y-m-d H:i:s'),
             'phoneNumberRaw' => $header['phoneNumberRaw'],
         ];
-        // Use WiFi password from label with ULV padding
-        $authCode = '1122334455';
+        // Use S/N from camera label with ULV padding
+        $authCode = '08820251212168';
         $this->devices[$phoneNumber]['authCode'] = $authCode;
 
         // Send registration response (0x8100) with result=0 and Terminal ID as auth code
