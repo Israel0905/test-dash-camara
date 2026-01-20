255875a8271f00ae90346b784761f672cd293c01
Harim Plata Solis <99371714+HarimSx@users.noreply.github.com>
2026-01-20 13:11:19 -0600

    changes

--- app/Services/MDVR/TcpServer.php
+++ app/Services/MDVR/TcpServer.php
@@ -288,7 +288,8 @@ class TcpServer
         ];
 
         // Generate simple ASCII auth code as requested
-        $authCode = "1234";
+        // Using terminal ID as auth code (Variation B)
+        $authCode = "000000992002";
         $this->devices[$phoneNumber]['authCode'] = $authCode;
 
         // Use raw phone bytes if available to ensure exact Terminal ID match
