0461e3df587602d69f15305ebf59109fbafa4a72
Israel0905 <omartoava75@gmail.com>
2026-01-20 10:33:16 -0600

    Padding 00 + Phone Number (992002)

--- app/Services/MDVR/TcpServer.php
+++ app/Services/MDVR/TcpServer.php
@@ -272,8 +272,8 @@ class TcpServer
         // Use Phone Number (992002) with Padding 00 format
         $authCode = $phoneNumber;
 
-        // Use Length Prefix + Terminal ID (device often requires auth code = its own ID)
-        $authCode = $terminalId; // "000000992002" - Length will be 12 (0x0C)
+        // Use Padding 00 + Phone Number (device might validate against phone, not terminal ID)
+        $authCode = $phoneNumber; // "992002" - Length will be 6
         $this->devices[$phoneNumber]['authCode'] = $authCode;
 
         $response = $this->messageBuilder->buildRegistrationResponseWithRawPhone(
@@ -284,7 +284,7 @@ class TcpServer
         );
         $this->sendResponse($connectionId, $response);
 
-        $this->log("Registration successful - Auth code: {$authCode} (with padding 0x00)");
+        $this->log("Registration successful - Auth code: {$authCode} (Padding 00 + Phone Number)");
     }
 
     /**
